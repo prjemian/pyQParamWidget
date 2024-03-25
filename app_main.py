086c1ebf98a1edbdd156561ec47489abfcc67e42
Pete R Jemian <prjemian@gmail.com>
2024-03-25 11:58:32 -0500

    working examples

---
+++ app_main.py
@@ -0,0 +1,41 @@
+"""Parameters Editor Demo"""
+import sys
+
+from PyQt5 import QtWidgets
+
+
+PARMS_FILE = "sampler.yml"
+PARMS_FILE = "example.yml"
+
+
+def read_parameter_specifications(parms_file):
+    """Read application parameter specifications from YAML file."""
+    import yaml
+    import pyqparamwidget as qpw
+
+    with open(parms_file) as f:
+        specs = yaml.load(f.read(), Loader=yaml.Loader)
+    parms = {}
+    for k, v in specs["parameters"].items():
+        args = [v["label"], v["default_value"]]
+        kwargs = {
+            kw: v[kw]
+            for kw in "widget choices hi lo tooltip".split()
+            if v.get(kw) is not None
+        }
+        parms[k] = qpw.ParameterItem(*args, **kwargs)
+    return parms
+
+
+def main():
+    from pyqparamwidget import ParameterEditorWidget
+
+    parms = read_parameter_specifications(PARMS_FILE)
+    app = QtWidgets.QApplication(sys.argv)
+    window = ParameterEditorWidget(None, parms)
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
---
+++ app_tree.py
@@ -0,0 +1,85 @@
+"""
+https://pythonspot.com/pyqt5-treeview/
+"""
+
+import sys
+
+from PyQt5 import QtWidgets
+import pyqparamwidget as qpw
+
+sampler = {
+    "tiled": {
+        "server": {
+            "settings_file": qpw.ParameterItem(
+                label="settings file", original_value="~/.config/settings.ini"
+            ),
+            "catalog": qpw.ParameterItem(
+                label="catalog", original_value="bluesky_data"
+            ),
+            "url": qpw.ParameterItem(label="url", original_value="http://localhost"),
+        },
+    },
+    "UI": {
+        "plotting": {
+            "autoplot": qpw.ParameterItem(
+                label="autoplot",
+                original_value=True,
+                widget=qpw.PARM_TYPE_CHECKBOX,
+                tooltip="Plot when the run is selected.",
+            ),
+            "autoselect": qpw.ParameterItem(
+                label="autoselect",
+                original_value=True,
+                widget=qpw.PARM_TYPE_CHECKBOX,
+                tooltip="Automatically select the signals to plot.",
+            ),
+            "colors": qpw.ParameterItem(label="colors", original_value="r b g k"),
+        },
+    },
+}
+# print(sampler)
+
+
+class DemoTreeWidget(QtWidgets.QTreeWidget):
+    """
+    Demo
+    """
+
+    def __init__(self, *args, parms={}, headings=["heading"], **kwargs):
+        from PyQt5.QtWidgets import QTreeWidgetItem
+
+        super().__init__(*args, **kwargs)
+        self.setColumnCount(len(headings))
+        self.setHeaderLabels(headings)
+        self.setSortingEnabled(True)
+
+        def isParmsDict(obj):
+            if isinstance(obj, dict):
+                for v in obj.values():
+                    if not isinstance(v, qpw.ParameterItem):
+                        return False
+                return True
+            return False
+
+        def build_subtree(parent, subparms):
+            if not isParmsDict(subparms):
+                for k, v in subparms.items():
+                    item = QTreeWidgetItem(None, [k])
+                    parent.addChild(item)
+                    build_subtree(item, v)
+
+        for k, v in parms.items():
+            item = QTreeWidgetItem(None, [k])
+            self.addTopLevelItems([item])
+            build_subtree(item, v)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = DemoTreeWidget(None, parms=sampler)
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
